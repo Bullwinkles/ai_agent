1027d3af74f06f4be74ce284e875233b8bf0c545
Bullwinkles <nolesfan7@gmail.com>
2025-08-12 17:06:36 -0400

    Added write_file function

--- tests.py
+++ tests.py
@@ -1,18 +1,15 @@
-from functions.get_file_content import *
+from functions.write_file import *
 
 
 def test():
-    result = get_file_content("calculator", "main.py")
-    print(result)
+   result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+   print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print(result)
+   result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+   print(result)
 
-    result = get_file_content("calculator", "bin/cat")
-    print(result)
-
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print(result)
+   result = write_file("calculator", "/tmp/tmp.txt", "this should not be allowed")
+   print(result)
 
 if __name__ == "__main__":
     test()
